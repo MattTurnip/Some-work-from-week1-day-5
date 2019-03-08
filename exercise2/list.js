86bcd933d1161390bfd574d1b56fed0e150cd830
MattTurnip <mattadavey@gmail.com>
2019-03-08 17:46:07 +0000

    did some stuff to sort a list

---
+++ exercise2/list.js
@@ -0,0 +1,29 @@
+/*
+Create a module that exports a function that takes a number as a parameter and stores it in a list. The list should not be accessible from outside the module.
+
+Export another function that returns a version of the data list that is sorted in ascending order. The function you use to sort the data correctly should not be accessible from outside the module. (Think back to the Custom Sorting Exercise when handling sorting)
+
+Implement a Node.js script that imports the functionality of your module, adds at least 5 different data points to the module's data list, and outputs the sorted list.
+*/
+
+var list = []
+
+module.exports = {
+    
+    
+    listAddFunc: function addToList(num) {
+        list.push(num);
+        return list;
+    },
+
+    sortAndPrint: function(){
+        list.sort(function(a, b) {
+            return a - b;
+          });
+        console.log(list);
+    }
+    
+}
+
+
+
---
+++ exercise2/tester.js
@@ -0,0 +1,10 @@
+var tester = require('./list');
+var input = process.argv.slice(2);
+
+tester.listAddFunc(1)
+tester.listAddFunc(11)
+tester.listAddFunc(4)
+tester.listAddFunc(7)
+
+
+tester.sortAndPrint();
